9001d365f169da4f4ba2b6683ee87e799547248f
eloranta <hannu.eloranta@gmail.com>
2025-08-29 07:08:47 +0300

    add layout

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,9 +1,32 @@
 #include "mainwindow.h"
+#include "waveformwidget.h"
+#include "QVBoxLayout"
+#include "QPushButton"
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
 {
+    auto *central = new QWidget(this);
+    setCentralWidget(central);
 
-}
+    auto *myWidget = new WaveformWidget(central);   // or: new QWidget(central);
+
+    auto *vbox = new QVBoxLayout(central);
+    vbox->setContentsMargins(8,8,8,8);
+    vbox->setSpacing(8);
+    vbox->addWidget(myWidget, /*stretch*/ 1); // take remaining space
+
+    auto *okBtn     = new QPushButton("OK", central);
+    auto *cancelBtn = new QPushButton("Cancel", central);
+    auto *btnRow = new QHBoxLayout();
+    btnRow->setSpacing(8);
+    btnRow->addStretch();
+    btnRow->addWidget(okBtn);
+    btnRow->addWidget(cancelBtn);
+
+    vbox->addLayout(btnRow); // add the horizontal row to the vertical layout
+
+    connect(okBtn, &QPushButton::clicked, this, []{ /* ... */ });
+ }
 
 MainWindow::~MainWindow()
 {
